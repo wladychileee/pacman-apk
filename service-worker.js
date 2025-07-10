30f506ae5425a8be8a35725d25664e86107d58b7
wladychileee <W_SALDIAS@HOTMAIL.COM>
2025-07-10 13:43:15 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -24,3 +24,14 @@ self.addEventListener('fetch', event => {
       .then(response => response || fetch(event.request))
   );
 });
+
+// Periodic Background Sync para mantener la cache actualizada
+self.addEventListener('periodicsync', (event) => {
+  if (event.tag === 'actualizar-cache') {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+    );
+  }
+});
